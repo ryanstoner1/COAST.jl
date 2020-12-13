6851aa2aa4647d340e92d1d98fedb34cd5c88a90
Ryan Stoner <rstoner@ucsb.edu>
2020-12-12 18:38:29 -0800

    Preprocess Th as well

--- he_preprocessing.jl
+++ he_preprocessing.jl
@@ -24,8 +24,14 @@ find concentration in units appropriate for RDAAM
     ppm_to_atoms_per_volume(U238::Float64;density=3.2)
 
 """
-function ppm_to_atoms_per_volume(U238::Float64;density=3.20)
+function ppm_to_atoms_per_volume(U238,Th232;density=3.20)
     U238_mol = U238/atomic_mass_U238 # mol/g
     U238_atoms_per_g = U238_mol*Na # atoms/g
     U238_V = U238_atoms_per_g*density # atoms/m^3
+
+    Th232_mol = Th232/atomic_mass_Th232 # mol/g
+    Th232_atoms_per_g = Th232_mol*Na # atoms/g
+    Th232_V = Th232_atoms_per_g*density # atoms/m^3
+
+    return U238_V,Th232_V
 end
