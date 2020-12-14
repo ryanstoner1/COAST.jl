f02c7d835d1ab66f7d0c883d6a1964b7115c0ea2
Ryan Stoner <rstoner@ucsb.edu>
2020-12-13 23:50:52 -0800

    Calculate moles He for input purposes

--- he_preprocessing.jl
+++ he_preprocessing.jl
@@ -9,10 +9,10 @@ Calculate *U* and *Th* concentrations from effective uranium and *Th/U* ratio
 
 
 """
-function decompose_eu(eU::Float64,Th_U_ratio::Float64)
+function decompose_eu(eU,Th_U_ratio)
 
-U = eU./(1.0.+0.24*Th_U_ratio)
-Th = eU./((1.0./Th_U_ratio).+0.24)
+U = eU./(1.0.+0.235*Th_U_ratio)
+Th = eU./((1.0./Th_U_ratio).+0.235)
 
 return U,Th
 end
@@ -33,5 +33,11 @@ function ppm_to_atoms_per_volume(U238,Th232;density=3.20)
     Th232_atoms_per_g = Th232_mol*Na # atoms/g
     Th232_V = Th232_atoms_per_g*density # atoms/m^3
 
-    return U238_V,Th232_V
+    return U238_V,Th232_V,U238_mol,Th232_mol
+end
+
+function UTh_date_to_He_mols(U,Th,date_ma)
+
+He_mols = 6*(Th*exp(lambda_32*1e6*sec_in_yrs*date_ma)-Th)+8*(U*exp(lambda_38*1e6*sec_in_yrs*date_ma)-U)
+return He_mols
 end
