a8313182984499d285f5216ae1eb5a17414431e1
Ryan Stoner <rstoner@ucsb.edu>
2020-12-13 23:54:00 -0800

    Make possible to input Th for constraints (forw. model). Input T0 too

--- he_vars_constraints.jl
+++ he_vars_constraints.jl
@@ -36,7 +36,7 @@ function define_variables!(n_t_segs,n_data_pts,model,start_point_frac;upper_boun
       upper_bound=map(constrain_upper,upper_bound[i],i,n_t_segs),
       start=map(constraint_func,i,maximum(upper_bound),maximum(lower_bound),start_point_frac[i]))
   end
-  @variable(model, T0==385.0)
+  @variable(model, T0==T0)
   @NLparameter(model, set_dev[i = 1:n_data_pts] == 0.0 * i)
 return T,T0,set_dev
 end
@@ -44,7 +44,7 @@ end
 """
 Let JuMP know about constraints
 """
-function rdaam_define_constraints(model,density,set_dev,set_val,L,times,T0,T,U238,U238_V;
+function rdaam_define_constraints(model,density,set_dev,set_val,L,times,T0,T,U238,Th232,U238_V,Th232_V;
    n_iter=20.0,E_L=122.3*1e3,c0=0.39528,c1=0.01073,c2=-65.12969,c3=-7.91715,
    alpha=0.04672,rmr0=0.79,D0_L2=exp(9.733),E_trap=34*1e3,omega_rho=1e-22,
    psi_rho=1e-13,L_dist=8.1*1e-4,eta_q=0.91)
@@ -55,11 +55,11 @@ kwargs_rdaam = (E_L,c0,c1,c2,c3,alpha,rmr0,D0_L2,E_trap,omega_rho,psi_rho,
 
 for j in eachindex(U238)
     if (j<n_data_constraints) || (n_data_constraints==1)
-      my_constr = @NLconstraint(model, [i = 1:1],(rdaam_forward_diffusion(density,n_iter,c0,c1,c2,c3,alpha,rmr0,U238_V[i],0.0,U238[i],0.0,E_L,L,T0,kappa,D0_L2,E_trap,omega_rho,psi_rho,eta_q,L_dist,times...,T...)-set_val[i]+set_dev[i])^2<=(set_val[i]*0.005)^2)
+      my_constr = @NLconstraint(model, [i = 1:1],(rdaam_forward_diffusion(density,n_iter,c0,c1,c2,c3,alpha,rmr0,U238_V[i],Th232_V[i],U238[i],Th232[i],E_L,L,T0,kappa,D0_L2,E_trap,omega_rho,psi_rho,eta_q,L_dist,times...,T...)-set_val[i]+set_dev[i])^2<=(set_val[i]*0.005)^2)
 
 
     elseif (j==n_data_constraints)
-      my_constr2 = @NLconstraint(model, [i = 2:2],(rdaam_forward_diffusion(density,n_iter,c0,c1,c2,c3,alpha,rmr0,U238_V[i],0.0,U238[i],0.0,E_L,L,T0,kappa,D0_L2,E_trap,omega_rho,psi_rho,eta_q,L_dist,times...,T...)-set_val[i]+set_dev[i])==(set_val[i]*0.0))
+      my_constr2 = @NLconstraint(model, [i = 2:2],(rdaam_forward_diffusion(density,n_iter,c0,c1,c2,c3,alpha,rmr0,U238_V[i],Th232_V[i],U238[i],Th232[i],E_L,L,T0,kappa,D0_L2,E_trap,omega_rho,psi_rho,eta_q,L_dist,times...,T...)-set_val[i]+set_dev[i])==(set_val[i]*0.0))
     end
 end
 
