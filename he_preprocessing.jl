31f38a836dfbe3811f006a33517012a34c0e2af6
Ryan Stoner <rstoner@ucsb.edu>
2020-12-17 05:39:31 -0800

    Add U35

--- he_preprocessing.jl
+++ he_preprocessing.jl
@@ -24,20 +24,29 @@ find concentration in units appropriate for RDAAM
     ppm_to_atoms_per_volume(U238::Float64;density=3.2)
 
 """
-function ppm_to_atoms_per_volume(U238,Th232;density=3.20)
+## preprocess
+function ppm_to_atoms_per_volume(U,Th232;density=3.20,U38_35_ratio=137.88)
+    U238=U*U38_35_ratio/(U38_35_ratio*(atomic_mass_U238/atomic_mass_U235)+1)
+    U235=U*1.0/(U38_35_ratio*(atomic_mass_U238/atomic_mass_U235)+1)
+
+
+    U235_mol = U235/atomic_mass_U235 # mol/g
+    U235_atoms_per_g = U235_mol*Na # atoms/g
+    U235_V = U235_atoms_per_g*density # atoms/cm^3
+
     U238_mol = U238/atomic_mass_U238 # mol/g
     U238_atoms_per_g = U238_mol*Na # atoms/g
-    U238_V = U238_atoms_per_g*density # atoms/m^3
+    U238_V = U238_atoms_per_g*density # atoms/cm^3
 
     Th232_mol = Th232/atomic_mass_Th232 # mol/g
     Th232_atoms_per_g = Th232_mol*Na # atoms/g
-    Th232_V = Th232_atoms_per_g*density # atoms/m^3
+    Th232_V = Th232_atoms_per_g*density # atoms/cm^3
 
-    return U238_V,Th232_V,U238_mol,Th232_mol
+    return U238_V,U235_V,Th232_V,U238_mol,U235_mol,Th232_mol
 end
 
-function UTh_date_to_He_mols(U,Th,date_ma)
+function UTh_date_to_He_mols(U38,U35,Th,date_ma)
 
-He_mols = 6*(Th*exp(lambda_32*1e6*sec_in_yrs*date_ma)-Th)+8*(U*exp(lambda_38*1e6*sec_in_yrs*date_ma)-U)
+He_mols = 6*(Th*exp(lambda_32*1e6*sec_in_yrs*date_ma)-Th)+8*(U38*exp(lambda_38*1e6*sec_in_yrs*date_ma)-U38)+7*(U35*exp(lambda_35*1e6*sec_in_yrs*date_ma)-U35)
 return He_mols
 end
