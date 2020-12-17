8212aaf597d504dbbf29f99608c05ffaf9e70368
Ryan Stoner <rstoner@ucsb.edu>
2020-12-17 05:39:01 -0800

    Restructure annealing component, add equivalent time. Tests from papers

--- src/he_forward.jl
+++ src/he_forward.jl
@@ -1,17 +1,22 @@
 # forward models of he diffusion
 # define constants
 
-const R_joules = 8.314 # gas constant
+const Rjoules = 8.314 # gas constant
 const sec_in_yrs = 3.1558e7
 const lambda_f = 8.46e-17/sec_in_yrs
 const lambda_38 = 1.55125 * 1e-10/sec_in_yrs
+const lambda_35 = 9.8584*1e-10/sec_in_yrs
 const lambda_32 = 4.9475*1e-11/sec_in_yrs
-const tau38 = 1.0./lambda_38
-const tau32 = 1.0./lambda_32
+const τ38 = 1.0./lambda_38
+const τ35 = 1.0./lambda_35
+const τ32 = 1.0./lambda_32
 
 const Na = 6.022e23  # avogadro number
+const atomic_mass_U235 = 235.04393 # g/mol
 const atomic_mass_U238 = 238.050791  # g/mol
 const atomic_mass_Th232 = 232.03805  # g/mol, CIAAW value
+
+
 
 """
 
@@ -21,113 +26,152 @@ Calculate radioactive ingrowth and diffusion of He4 using modified eqns
 # Arguments:
 - `density::Float64`: the .
 """
-function rdaam_forward_diffusion(density,n_iter,c0,c1,c2,c3,alpha,rmr0,U238_V,Th232_V,U238,
-  Th232,E_L,L,T0,kappa,D0_L2,E_trap,omega_rho,psi_rho,eta_q,L_dist,t_and_T...)#::Tvv...) where {Tvv<:Real}
+function rdaam_forward_diffusion(alpha,c0,c1,c2,c3,rmr0,eta_q,L_dist,psi,omega,Etrap,
+  R,Ea,logD0_a2,n_iter,U238,U238_V,U235,U235_V,Th232,Th232_V,L,times,
+  T...)
 
-n_t = length(t_and_T)/2
-n_t = ceil(Int64,n_t)
-times = t_and_T[1:n_t]
-T = t_and_T[n_t+1:end]
-Tvv = eltype(T)
+  kappa = 1.04 - rmr0
+  rdaam2nd_root = 0.5274 # cutoff bellow which rho_r values can be negative
+  rdaam2nd_root_cutoff = (rdaam2nd_root^(1/kappa))*(1-rmr0)+rmr0 # also prevents
+  # square root term from being negative
 
-# preallocate
-rho_r=0.0
-erho_s=0.0
-N_t_segs = length(T)+1
-D0_rdaam = zeros(Tvv,N_t_segs+1)
-F = zeros(Tvv,N_t_segs+1)
-zeta = zeros(Tvv,N_t_segs+1)
-dzeta = zeros(Tvv,N_t_segs+1)
-dfdchi = zeros(Tvv,(N_t_segs-1))
-a = 0.0
+
+  # diffusivity mod
+
+  # preallocate
+  (t_equiv,N_t_segs,e_rho_s,rcb2,rho_r,D0,F,zeta,dzeta,dfdchi) = preallocate_diff_data(T) # 1st arg is only for rad dam
+
+
+
+  n_times = length(times)
+  dt_equiv = 0.0
+  for rev_ind in eachindex(times) # present to past
+    if rev_ind>1
+        t_dam = times[n_times-rev_ind+1]-times[n_times-rev_ind+2]+dt_equiv
+        rcb2_base = (c0 + c1*(log(t_dam)-c2)/(log((1.0/T[n_times-rev_ind+1]))-c3))
+        rcb2_fill_val = ((rcb2_base^(1.0/alpha))+1)^(-1)
+        for jj in 1:(n_times-rev_ind+1)
+            rcb2[rev_ind+jj-1,jj+1] = rcb2_fill_val
+            if rcb2[rev_ind+jj-1,jj+1]>= rdaam2nd_root_cutoff
+                rho_r[rev_ind+jj-1,jj+1] = ((rcb2_fill_val-rmr0)/(1.0-rmr0))^kappa
+                rho_r_copy = copy(rho_r[rev_ind+jj-1,jj+1]) # otherwise if AND elseif
+
+                if rho_r_copy>=0.765
+                   rho_r[rev_ind+jj-1,jj+1] = 1.6*rho_r[rev_ind+jj-1,jj+1] - 0.6
+                elseif rho_r_copy<0.765
+                   rho_r[rev_ind+jj-1,jj+1] = 9.205*rho_r[rev_ind+jj-1,jj+1]^2 - 9.157*rho_r[rev_ind+jj-1,jj+1] + 2.269
+                end
+            end
+        end
+        # use concept of equivalent time
+      if rev_ind<n_times
+        dt_equiv = exp((((((1.0/rcb2[rev_ind,2])-1.0)^alpha-c0)*(log(1.0/T[n_times-rev_ind])-c3))/c1)+c2)
+      end
+    end
+  end
 
 U238_0 = U238*(exp(lambda_38*times[1])) # U238 is measured at present
-Th232_0 = Th232*(exp(lambda_32*times[1]))
-# calculate apatite grain damage and annealing
-delt_rad = 4e6*sec_in_yrs
-t_rad = collect(1270*1e6*sec_in_yrs:-delt_rad:1e6*sec_in_yrs)
-
-for (ind,time_i) in enumerate(t_rad)
-  (~,ind_save) = findmin(abs.(times.-time_i))
-  if ind_save>1
-    a = ((c0+c1*(log(t_rad[ind-1]-time_i)-c2)/(log(1/T[ind_save-1])-c3))^(1/alpha)+1)^(-1)
-  else
-    a = ((c0+c1*(log(sec_in_yrs)-c2)/(log(1/T0)-c3))^(1/alpha)+1)^(-1)
-  end
-  rclr=((a-rmr0)/(1-rmr0))^kappa
-  if rclr>0.765
-    rho_r = 1.6*rclr-0.6
-  elseif rclr<=0.765
-    rho_r = 9.205*rclr^2-9.157*rclr+2.269
-  end
-
-  if ind_save==1
-    rho_v=0.0#+(6/8)*Th232_V*(exp(lambda_32*(time_i))-exp(lambda_32*(time_i-sec_in_yrs)))
-  else
-    rho_v=(8/8)*U238_V*(exp(lambda_38*(t_rad[ind-1]))-exp(lambda_38*(time_i)))#+(6/8)*Th232_V*(exp(lambda_32*(times[ind-1]))-exp(lambda_32*(time_i)))
-    rho_v+=(6/8)*Th232_V*(exp(lambda_32*(t_rad[ind-1]))-exp(lambda_32*(time_i)))
-  end
-
-  erho_s += eta_q*rho_v*rho_r*L_dist*lambda_f/lambda_38
-  if ind_save==1
-  D0_L2_rdaam = 1e-4 #((D0_L2*exp(-E_L./(R_joules*T0)))/
-  #         (exp(E_trap/(R_joules*T0))*(psi_rho*erho_s+omega_rho*erho_s^3)+1))
-  else
-    D0_L2_rdaam = ((D0_L2*exp(-E_L/(R_joules*T[ind_save-1])))/
-            (exp(E_trap/(R_joules*T[ind_save-1]))*(psi_rho*erho_s+omega_rho*erho_s^3)+1))
-  end
-  D0_rdaam[ind_save] = D0_L2_rdaam*L^2
-  if ind_save>1
-       dzeta_val = t_rad[ind-1]-t_rad[ind]
-       #print("dzeta diff is: $dzeta_val \n")
-       dzeta[ind_save] = (D0_rdaam[ind_save-1]+D0_rdaam[ind_save])*(t_rad[ind-1]-t_rad[ind])/2
-       zeta[ind_save] = zeta[ind_save-1]+dzeta[ind_save]
-       F[ind_save] = 8*(U238_0/tau38)*tau38*(1.0-exp(-t_rad[ind]/tau38))
-       F[ind_save] += 6*(Th232_0/tau32)*tau32*(1.0-exp(-t_rad[ind]/tau32))
+U235_0 = U235*(exp(lambda_35*times[1])) # U238 is measured at present
+Th232_0 = Th232*(exp(lambda_32*times[1])) # U238 is measured at present
 
 
-  end
+  ## prep diffusivity input
+  for ind in eachindex(times)
+       if ind>1
+         update_damage!(times,eta_q,L_dist,U238_V,U235_V,Th232_V,e_rho_s,ind,rho_r)
+       end
 
-  if (ind_save<N_t_segs+1) & (ind_save>1)
-    dfdchi[ind_save-1]=(F[ind_save-1]-F[ind_save])/(dzeta[ind_save])
-  end
+
+
+      # ## tested
+      F[ind] = 8*(U238_0)*(-exp(-times[ind]/τ38))
+      F[ind] += 7*(U235_0)*(-exp(-times[ind]/τ35))
+      F[ind] += 6*(Th232_0)*(-exp(-times[ind]/τ32))
+      if ind>1
+        damage_mod_term = ((psi*e_rho_s[ind] + omega*e_rho_s[ind]^3)*exp(Etrap/(R*T[ind-1])))+1
+        D0[ind] = arrhenius(L,T[ind-1],R,Ea,logD0_a2)/damage_mod_term
+        dzeta_val = times[ind-1]-times[ind]
+        dzeta[ind] = (D0[ind-1]+D0[ind])*(times[ind-1]-times[ind])/2
+        zeta[ind] = zeta[ind-1]+dzeta[ind]
+
+
+      else
+        damage_mod_term = ((psi*e_rho_s[ind] + omega*e_rho_s[ind]^3)*exp(Etrap/(R*T[1])))+1
+        D0[ind] = arrhenius(L,T[1],R,Ea,logD0_a2)/damage_mod_term
+      end
+
+      if (ind<N_t_segs+1) & (ind>1)
+         dfdchi[ind-1]=(F[ind-1]-F[ind])/(dzeta[ind])
+      end
+
+    end
+
+   zeta_end = zeta[end]
+
+    uterm = fill_u_term(L,n_iter,N_t_segs,F,dfdchi,dzeta,zeta,zeta_end)
+
+
+    uF = uterm*(8)/pi
+    uF = (uF/(pi*4/3))
+
+     return uF
 
 end
+# end rdaam
 
+## calc new damage
+function update_damage!(times,eta_q,L_dist,U238_V,U235_V,Th232_V,e_rho_s,ind,rho_r_final)
 
-uterm2 = 0.0
-mu_n = 0.0
+  dt = times[ind-1]-times[ind]
 
-zeta_end = zeta[end-1]
+  rho_v = (8/8)*U238_V*(exp(lambda_38*times[ind-1])-exp(lambda_38*times[ind]))
+  rho_v += (7/8)*U235_V*(exp(lambda_35*times[ind-1])-exp(lambda_35*times[ind]))
+  rho_v += (6/8)*Th232_V*(exp(lambda_32*times[ind-1])-exp(lambda_32*times[ind]))
 
+  e_rho_s[ind]= 1.0*rho_v*sum(rho_r_final[ind,:])*eta_q*L_dist*lambda_f/lambda_38
 
-uterm2 = fill_u_term2(L,n_iter,N_t_segs,F,dfdchi,dzeta,zeta,mu_n,uterm2,zeta_end)
-
-
-
-uF = uterm2*(8)/pi
-
-uF = uF/(pi*4/3) # [uF] = ppm - [He] @ modern day/after ingrowth
-#zerpl = D0_rdaam[1]
-#print("$uF \n")
-
-return uF
+  return nothing
 end
 
-function fill_u_term2(L,n_iter,N_t_segs,F,dfdchi,dzeta,zeta,mu_n,uterm2,zeta_end)
-    mu_base = (pi/L)^2
+function arrhenius(L,T,R,Ea,logD0_a2)
+
+  D = 10^(logD0_a2)*L^2.0.*exp(-Ea./(R*T))
+
+  return D
+end
+
+function fill_u_term(L,n_iter,N_t_segs,F,dfdchi,dzeta,zeta,zeta_end)
+  uterm = 0.0
+  mu_n = 0.0
   for n in 1:n_iter
           utermbase = 0.0
-          mu_n = mu_base*n^2
+          mu_n = (pi/(L))^2*n^2
           for j in 1:(N_t_segs-1)
-               #dfdchi = (F[j+1]-F[j])/(dzeta[j+1])
-               #if mu_n*(sum(dzeta[j+2:end]))>=1e-14
-              utermbase+= dfdchi[j]*(exp(-mu_n*(zeta_end-zeta[j+1]))-exp(-mu_n*(zeta_end-zeta[j])))
-           end
 
-           uterm2 += (1/mu_n)*(1/(n*n))*utermbase #(-fx)*(exp(-t[]/tau)-exp(-k*(n*pi/L)^2*t))/(1-tau*k*(n*pi/L)^2) #cos.(n_term*r./L)*(1-exp(-t*k*(n_term/L)^2))*fn_t/(k*(n_term/L)^2)
+             utermbase+= dfdchi[j]*(exp(-mu_n*(zeta_end-zeta[j+1]))-exp(-mu_n*(zeta_end-zeta[j])))
+
+          end
+          uterm += (1/mu_n)*(1/(n*n))*utermbase
+
   end
-  return uterm2
+  return uterm
+end
+
+function preallocate_diff_data(T)
+
+  Tvv = eltype(T)
+  N_t_segs = length(T)+1
+  e_rho_s= zeros(Tvv,N_t_segs)
+  t_equiv = zeros(N_t_segs)
+  D0 = zeros(Tvv,N_t_segs)
+  F = zeros(N_t_segs)
+  zeta = zeros(Tvv,N_t_segs)
+  dzeta = zeros(Tvv,N_t_segs)
+  dfdchi = zeros(Tvv,(N_t_segs-1))
+  rcb2 = zeros(Tvv,(N_t_segs,N_t_segs))
+  rho_r = zeros(Tvv,(N_t_segs,N_t_segs))
+
+  return t_equiv,N_t_segs,e_rho_s,rcb2,rho_r,D0,F,zeta,dzeta,dfdchi
 end
 
 function mod_constraints(T0,T::Tvv...) where {Tvv<:Real}
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,82 @@ using Test
 @testset "COAST.jl" begin
     @test loaded_COAST()==true
 end
+
+@testset "he_forward.jl" begin
+    ## input
+    # rdaam params
+    # eqs, fanning curvilinear, ketcham et al 07
+    alpha = 0.04672
+    c0 = 0.39528
+    c1 = 0.01073
+    c2 = -65.12969
+    c3 =  -7.91715
+    rmr0 = 0.79
+    eta_q = 0.91
+    L_dist = 8.1*1e-4 # cm (!)
+
+    # general params
+    U238 = 28*1e-6
+    R = 1.9872*1e-3
+    L = 60*1e-4 # m
+    n_T_pts = 30
+    n_iter = 30.0
+
+    # preprocess concentrations
+    (U238_V,U235_V,Th232_V,U238_mol,U235_mol,Th232_mol) = ppm_to_atoms_per_volume(U238,0.0,density=3.20)
+
+    """
+    check if RDAAM reproduces vanilla Durango
+    from farley et al, 00; fig. 1
+    """
+    L1 = 90*1e-4
+    logD0_a2 = log10(10^1.5/(L1^2)) # cm^2/s
+    Ea = 32.9
+
+    T = collect(LinRange(120.0,0.01,ceil(Int,n_T_pts)).+273.15)
+    times = collect(LinRange(120.0,0.01,ceil(Int,n_T_pts)+1).*3.1558e7*1e6)
+    mass_he_t1 = rdaam_forward_diffusion(alpha,c0,c1,c2,c3,rmr0,eta_q,L_dist,0.0,0.0,0.0,R,Ea,logD0_a2,n_iter,
+                  U238_mol,U238_V,U235_mol,U235_V,Th232_mol,Th232_V,L,times,T...)
+    pre_he_t1 = (8*(U238_mol*exp(55*1e6*sec_in_yrs/τ38)-U238_mol)+
+                 7*(U235_mol*exp(55*1e6*sec_in_yrs/τ35)-U235_mol)) # Dodson value
+    @test isapprox(mass_he_t1/pre_he_t1,1.0; atol = 6e-2)
+
+    """
+    calculate erho
+    rdaam params in diffn
+    test fig 2, 2009 RDAAM
+    """
+    Rjoules = 8.314
+    psi = 1e-13
+    omega = 1e-22
+    Etrap = 34*1e3 # J/mol
+    E_L = 122.3*1e3 # J/mol
+    L2 = 60*1e-4
+    log10D0L_a2_rdaam = log10(exp(9.733)*L^2/L2^2)
+
+    T = collect(LinRange(120.0,0.0,ceil(Int,n_T_pts)).+273.15)
+    times = collect(LinRange(120.0,0.01,ceil(Int,n_T_pts)+1).*3.1558e7*1e6)
+    mass_he_t2 = rdaam_forward_diffusion(alpha,c0,c1,c2,c3,0.83,eta_q,L_dist,psi,omega,Etrap,Rjoules,E_L,log10D0L_a2_rdaam,n_iter,
+                   U238_mol,U238_V,U235_mol,U235_V,Th232_mol,Th232_V,L,times,T...)
+    pre_he_t2 = (8*(U238_mol*exp(59*1e6*sec_in_yrs/τ38)-U238_mol)+
+                 7*(U235_mol*exp(59*1e6*sec_in_yrs/τ35)-U235_mol))# Dodson value
+    @test isapprox(mass_he_t2/pre_he_t2,1.0; atol = 3e-2)
+
+    """
+    wolf,98 fig. 5 - p3
+    """
+    T1 = collect(LinRange(60.0,60.0,ceil(Int,n_T_pts/2)).+273.15)
+    T2 = collect(LinRange(15.0,15.0,floor(Int,n_T_pts/2)).+273.15)
+    T = vcat(T1,T2)
+    times1 = collect(LinRange(100.0,20.0,ceil(Int,n_T_pts/2)+1).*3.1558e7*1e6)
+    times2 = collect(LinRange(19.9,0.01,floor(Int,n_T_pts/2)).*3.1558e7*1e6)
+    times = vcat(times1,times2)
+    mass_he_t3 = rdaam_forward_diffusion(alpha,c0,c1,c2,c3,0.83,eta_q,L_dist,
+                                         0.0,0.0,0.0,R,Ea,logD0_a2,n_iter,
+                                         U238_mol,U238_V,U235_mol,U235_V,
+                                         Th232_mol,Th232_V,L,times,T...)
+    pre_he_t3 = (8*(U238_mol*exp(40*1e6*sec_in_yrs/τ38)-U238_mol)+
+                 7*(U235_mol*exp(40*1e6*sec_in_yrs/τ35)-U235_mol))
+    @test isapprox(mass_he_t3/pre_he_t3,1.0; atol = 4e-2)
+
+end
