7eba5191c90533319deb6da69156e31c3677a1a3
Ryan Stoner <rstoner@ucsb.edu>
2021-01-25 22:25:44 -0800

    Add venv for python

--- app/coast_app.py
+++ app/coast_app.py
@@ -5,23 +5,6 @@ Created on Wed Dec 30 16:38:11 2020
 
 @author: ryanstoner
 """
-import streamlit as st
-from SessionState import get
-
-session_state = get(password='')
-def main():
-    st.title("hey")
-    uploaded_files = st.file_uploader("Choose a CSV file")
-    return 
-
-if session_state.password != 'pwd123':
-    pwd_placeholder = st.sidebar.empty()
-    pwd = pwd_placeholder.text_input("Password:", value="", type="password")
-    session_state.password = pwd
-    if session_state.password == 'pwd123':
-        pwd_placeholder.empty()
-        main()
-    elif session_state.password != '':
-        st.error("the password you entered is incorrect")
-else:
-    main()
+import dash
+import os
+print(os.getcwd())
