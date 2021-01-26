584409664c86a14eeb1b43c700484420577819c8
Ryan Stoner <rstoner@ucsb.edu>
2021-01-25 21:38:30 -0800

    indentation fixed

--- FT_forward.jl
+++ FT_forward.jl
@@ -1,15 +1,17 @@
 
 function FT_forward(alpha,beta,c0,c1,c2,c3,T,times)
-n_times = length(times)
-dt_equiv = 0.0
-    for ind in eachindex(times)
-        if ind>1
-            t_dam = times[n_times-ind+1]-times[n_times-ind+2]+dt_equiv
-            rcb2_base = (c0 + c1*(log(t_dam)-c2)/(log((1.0/T[n_times-ind+1]))-c3))
-            rcb2_fill_val = ((rcb2_base^(1.0/alpha))+1)^(-1)
-        end
-    end
 
+    n_times = length(times)
+    dt_equiv = 0.0
+    rcb2_base = 0.0
+        for ind in eachindex(times)
+            if ind>1
+                t_dam = times[n_times-ind+1]-times[n_times-ind+2]+dt_equiv
+                rcb2_base = (c0 + c1*(log(t_dam)-c2)/(log((1.0/T[n_times-ind+1]))-c3))
+                rcb2_fill_val = ((rcb2_base^(1.0/alpha))+1)^(-1)
+            end
+        end
+    return rcb2_base
 end
 
 
