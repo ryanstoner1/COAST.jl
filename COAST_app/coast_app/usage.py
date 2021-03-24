bf8a5ee648514a85c505990bb9a42ac8282bf0ec
ryanstoner1 <rstoner@ucsb.edu>
2021-03-24 11:00:07 -0700

    Add basic Dash authentication

--- COAST_app/coast_app/usage.py
+++ COAST_app/coast_app/usage.py
@@ -1,9 +1,19 @@
 import coast_app
 import dash
+import dash_auth
 from dash.dependencies import Input, Output
 import dash_html_components as html
 
+# Keep this out of source code repository - save in a file or a database
+VALID_USERNAME_PASSWORD_PAIRS = {
+    'hello': 'world'
+}
+
 app = dash.Dash(__name__)
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
 
 app.layout = html.Div([
     coast_app.CoastApp(
