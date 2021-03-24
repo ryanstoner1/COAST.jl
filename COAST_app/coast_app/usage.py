070b0d5d13e587137ee90cd4e9a70ce01f73b799
ryanstoner1 <rstoner@ucsb.edu>
2021-03-24 11:11:45 -0700

    Re-add basic graph functionality

--- COAST_app/coast_app/usage.py
+++ COAST_app/coast_app/usage.py
@@ -3,10 +3,11 @@ import dash
 import dash_auth
 from dash.dependencies import Input, Output
 import dash_html_components as html
+import dash_core_components as dcc
 
 # Keep this out of source code repository - save in a file or a database
 VALID_USERNAME_PASSWORD_PAIRS = {
-    'hello': 'world'
+    'thermo': 'Apatite3'
 }
 
 app = dash.Dash(__name__)
@@ -15,19 +16,26 @@ auth = dash_auth.BasicAuth(
     VALID_USERNAME_PASSWORD_PAIRS
 )
 
-app.layout = html.Div([
-    coast_app.CoastApp(
-        id='input',
-        value='my-value',
-        label='my-label'
-    ),
-    html.Div(id='output')
+app.layout = html.Div([dcc.Tabs(id='tabs-example', value='tab-1', children=[
+    dcc.Tab(label='He Diffusion', children=[
+        coast_app.CoastApp(
+            id='input',
+            value='my-value',
+            label='[10,20]'
+        ),
+        html.Div(["Input: ",
+                  dcc.Input(id='my-input', value=90, type='number')]),
+        html.Div(id='output'),
+        # Hidden div inside the app that stores the intermediate value
+        html.Div(id='intermediate-value', style={'display': 'none'}),
+        html.Div(id='graph')]),
+    dcc.Tab(label='AFT', value="tab dos"),
+    dcc.Tab(label='Vitrinite', value="tab-2", children=[
+        dcc.Tab(label='foo', value="tab tres"),
+        dcc.Tab(label='bar', value="tab quatro")
+    ]
+    )])
 ])
-
-
-@app.callback(Output('output', 'children'), [Input('input', 'value')])
-def display_output(value):
-    return 'You have entered {}'.format(value)
 
 
 if __name__ == '__main__':
