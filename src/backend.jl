56a2225cb574a461d60fce959bc842052e1be209
ryanstoner1 <rstoner@ucsb.edu>
2021-03-24 15:34:21 -0700

    Setup backend for website

---
+++ src/backend.jl
@@ -0,0 +1,31 @@
+using Genie
+using Genie.Router
+using Genie.Requests
+using Genie.Renderer
+Genie.config.cors_headers["Access-Control-Allow-Methods"] ="GET,POST"
+function launchServer(port)
+
+    Genie.config.run_as_server = true
+    Genie.config.server_host = "0.0.0.0"
+    Genie.config.server_port = port
+
+    println("port set to $(port)")
+
+    route("/") do
+        "COAST: Error"
+    end
+
+route("/", method = POST) do
+  @show jsonpayload()
+  @show rawpayload()
+  aa = jsonpayload()["name"] 
+  ab = jsonpayload()["lastName"] 
+  
+  json("Mr.$(aa) $(ab) ")
+  
+end
+
+    Genie.AppServer.startup()
+end
+
+launchServer(parse(Int, ARGS[1]))
