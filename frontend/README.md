876fd28c081b70117f7a04f282450e0a0a9f0c45
ryanstoner1 <rstoner@ucsb.edu>
2021-03-26 03:18:40 -0700

    rename folders to reflect their function

---
+++ frontend/README.md
@@ -0,0 +1,3 @@
+# COAST_app
+app for thermochronology package COAST
+running at https://coast.thermochron.org
---
+++ frontend/coast_app/R/internal.R
@@ -0,0 +1,15 @@
+.coastApp_js_metadata <- function() {
+deps_metadata <- list(`coast_app` = structure(list(name = "coast_app",
+version = "0.0.1", src = list(href = NULL,
+file = "deps"), meta = NULL,
+script = 'coast_app.min.js',
+stylesheet = NULL, head = NULL, attachment = NULL, package = "coastApp",
+all_files = FALSE), class = "html_dependency"),
+`coast_app` = structure(list(name = "coast_app",
+version = "0.0.1", src = list(href = NULL,
+file = "deps"), meta = NULL,
+script = 'coast_app.min.js.map',
+stylesheet = NULL, head = NULL, attachment = NULL, package = "coastApp",
+all_files = FALSE, dynamic = TRUE), class = "html_dependency"))
+return(deps_metadata)
+}
