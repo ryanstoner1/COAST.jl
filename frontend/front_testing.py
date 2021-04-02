50d41257648d709e99bbcca8c0e482ef2e9995d7
ryanstoner1 <rstoner@ucsb.edu>
2021-04-02 01:28:16 -0700

    clean testing; test sensitivity started

--- frontend/front_testing.py
+++ frontend/front_testing.py
@@ -3,9 +3,12 @@ from SALib.analyze import sobol
 from SALib.test_functions import Ishigami
 import numpy as np
 import base64
-from coast_app import txtextract
+import chaospy
+from coast_app import txt_read_preprocess
+from coast_app import sensitivity_analysis
+from matplotlib import pyplot as plt
 import unittest 
-
+import scipy
 class UsageTesting(unittest.TestCase):
 
     def test_always_passes(self):
@@ -21,56 +24,35 @@ class UsageTesting(unittest.TestCase):
         default_envelope_ind = 6
         
         print("\n starting HeFTy loading tests! \n")
-        (bound_box, n_extra_constraints, decoded_shortened) = txtextract.extract_Tt_constraints(default_envelope_ind,decoded)
+        (bound_box, n_extra_constraints, decoded_shortened) = txt_read_preprocess.extract_Tt_constraints(default_envelope_ind,decoded)
         
         self.assertTrue(bound_box[0][0] == 54.2769230769231)
 
-        (good_acc_bounds, decoded_shortened) = txtextract.extract_Tt_bounds(decoded_shortened)
+        (good_acc_bounds, decoded_shortened) = txt_read_preprocess.extract_Tt_bounds(decoded_shortened)
         (good_time, good_hi, good_lo, acc_time, acc_hi, acc_lo) = good_acc_bounds
 
         self.assertTrue(good_time[0] == 53.7312462538045)
 
-        (dates, acc_temp_interp, good_temp_interp) = txtextract.interp_Tt_finer_scale(
+        (dates, acc_temp_interp, good_temp_interp) = txt_read_preprocess.interp_Tt_finer_scale(
             good_time, acc_time, decoded_shortened)
 
+    def test_sensitivity_analysis_wout_backend(self):
+        f =  open("testfiles/mo-inv.txt","r") 
+        decoded_raw = f.read()
+        f.close()
+        decoded = str(decoded_raw).splitlines()
+        decoded = np.array(decoded)
+        default_envelope_ind = 6
+        
+        print("\n starting sensitivity tests! \n")
 
-# # Define the model inputs
-# problem = {
-#     'num_vars': 3,
-#     'names': ['x1', 'x2', 'x3'],
-#     'bounds': [[-3.14159265359, 3.14159265359],
-#                [-3.14159265359, 3.14159265359],
-#                [-3.14159265359, 3.14159265359]]
-# }
-
-# # Generate samples
-# param_values = saltelli.sample(problem, 1000)
-# # Run model (example)
-# Y = Ishigami.evaluate(param_values)
-
-# # Perform analysis
-# Si = sobol.analyze(problem, Y, print_to_console=False)
-
-# # Print the first-order sensitivity indices
-# print(Si['S1'])
-
-# # 
-
-# #
-# # Define the model inputs
-# problem2 = {
-#     'num_vars': 1,
-#     'names': ['x1'],
-#     'bounds': [[-3.14159265359, 3.14159265359]]
-# }
-
-# # Generate samples
-# param_values2 = saltelli.sample(problem2, 7500)
-# param_values2 = param_values2.flatten()
-# noise = np.random.rand(30000)
-
-# print(param_values2)
-# Y2 = param_values2*1.0*noise
+        # same as test 2 to setup
+        (bound_box, n_extra_constraints, decoded_shortened) = txt_read_preprocess.extract_Tt_constraints(default_envelope_ind,decoded)
+        (good_acc_bounds, decoded_shortened) = txt_read_preprocess.extract_Tt_bounds(decoded_shortened)
+        (good_time, good_hi, good_lo, acc_time, acc_hi, acc_lo) = good_acc_bounds
+        (dates, acc_temp_interp, good_temp_interp) = txt_read_preprocess.interp_Tt_finer_scale(
+            good_time, acc_time, decoded_shortened)
+        # end same as test 2
 
 # # Perform analysis
 # Si2 = sobol.analyze(problem2, Y2, print_to_console=True)
