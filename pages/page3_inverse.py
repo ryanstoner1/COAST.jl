2b1e5098a777a72f909d901af323b5318ad729d8
ryanstoner1 <rstoner@ucsb.edu>
2021-04-11 13:13:03 -0700

    Split frontend into individual pages

---
+++ pages/page3_inverse.py
@@ -0,0 +1,194 @@
+from app import app
+import json
+import dash_table
+import pandas as pd
+import numpy as np
+from collections import OrderedDict
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+import sensitivity_analysis
+from dash.exceptions import PreventUpdate
+from dash.dependencies import Input, Output, State
+d1 = pd.DataFrame(OrderedDict([
+    ('distance', [0]*29),
+    ('date', [0]*29),
+    ('error', [0]*29),
+]))
+
+df2 = pd.DataFrame(OrderedDict([
+    ('distance', [0]*29),
+    ('date', np.zeros(29)),
+    ('error', np.zeros(29)),
+]))
+
+df3 = pd.DataFrame(OrderedDict([
+    ('distance', [0]*29),
+    ('date', np.zeros(29)),
+    ('error', np.zeros(29)),
+]))
+
+page_3_layout = html.Div([
+    html.H1('Inverse modeling'),
+    dbc.FormGroup(
+        [
+            dbc.Label("Diffusion Activation energy"),
+            html.Div([ 
+                html.Span("D",style = {'display': 'inline-block','margin-left':'10px'}), 
+                html.Sub('0'),
+                dcc.Input(placeholder=" . . .", id="D0-in", min=0, max=1e8,style = {'display': 'block','margin-left':'10px','width': '5em'}),
+                html.Span("E",style = {'display': 'inline-block','margin-left':'10px'}), 
+                html.Sub('a'),
+                dcc.Input(placeholder="Ea (kJ)", id="Ea-in", min=0, max=1e8,style = {'display': 'block','margin-left':'10px','width': '5em'}),
+            ],style = {'display': 'block'}),
+            html.Div([
+                html.Span("n",style = {'display': 'inline-block','margin-left':'10px'}), 
+                html.Sub('samples'),
+                dcc.Input(placeholder="number", id="n-samp", min=0, max=1e8,style = {'display': 'block','margin-left':'10px','width': '5em'}),
+                html.Span('\u0394',style = {'display': 'inline-block','margin-left':'10px'}),
+                html.Sub('r'),
+                dcc.Input(placeholder="time", id="time", min=0, max=1e8,style = {'display': 'block','margin-left':'10px','width': '5em'}),
+            ],style = {'display': 'block'}),
+            dbc.FormText("Type the activation energy in the box above"),
+        ]
+    ),
+    dbc.Button("Process Data", id="table-button", className="mr-2"),
+    html.Span(id="example-output", style={"vertical-align": "middle"}),
+    html.Br(),
+    dbc.Button("Process PCE", id="pce-button", className="mr-1"),
+    html.Span(id="pce-output", style={"vertical-align": "middle"}),
+    html.H2("GB119C-10"),
+    dash_table.DataTable(
+        id='t1',
+        data=d1.to_dict('records'),
+        columns=[{
+            'id': 'distance',
+            'name': 'distance',
+            'type': 'numeric'
+        }, {
+            'id': 'date',
+            'name': 'date',
+            'type': 'numeric'
+        }, {
+            'id': 'error',
+            'name': 'error',
+            'type': 'numeric'
+        }],
+        editable=True
+    ),
+    html.Div(id='t1_out'),
+    html.H2("GB119C-32"),
+    dash_table.DataTable(
+        id='t2',
+        data=df2.to_dict('records'),
+        columns=[{
+            'id': 'distance',
+            'name': 'distance',
+            'type': 'numeric'
+        },{
+            'id': 'date',
+            'name': '06/38',
+            'type': 'numeric'
+        }, {
+            'id': 'error',
+            'name': '2 sigma (abs)',
+            'type': 'numeric'
+        }],
+        editable=True
+    ),
+    html.H2("GB119C-42"),
+    dash_table.DataTable(
+        id='t3',
+        data=df3.to_dict('records'),
+        columns=[{
+            'id': 'distance',
+            'name': 'distance',
+            'type': 'numeric'
+        },{
+            'id': 'date',
+            'name': '06/38',
+            'type': 'numeric'
+        }, {
+            'id': 'error',
+            'name': '2 sigma (abs)',
+            'type': 'numeric'
+        }],
+        editable=True
+    ),    
+])
+
+# output the stored clicks in the table cell.
+@app.callback(Output('D0-in', 'value'),
+                Output('Ea-in', 'value'),
+                # Since we use the data prop in an output,
+                # we cannot get the initial data on load with the data prop.
+                # To counter this, you can use the modified_timestamp
+                # as Input and the data as State.
+                # This limitation is due to the initial None callbacks
+                # https://github.com/plotly/dash-renderer/pull/81
+                Input('session', 'modified_timestamp'),
+                State('session', 'data'))
+def on_data(ts, data):
+    if ts is None:
+        raise PreventUpdate
+
+    data = data or {}
+
+    return data.get('D0', 0),data.get('Ea', 0)
+
+
+
+@app.callback(
+    Output("pce-output", "children"), [Input("pce-button", "n_clicks")],Input('n-samp',"value")
+)
+def on_button_click(n,nsamp):
+    if n is None:
+        return "Not clicked."
+    else:
+        nsamp = int(nsamp)
+        in_means = [20.0,10.0]
+        in_stds = [20.0,10.0]
+        distributions = sensitivity_analysis.get_input_distributions(in_means, in_stds)
+        samples = distributions.sample(nsamp, rule="halton")
+        # evaluations = numpy.array([exponential_model(sample,coordinates=coordinates)
+        #     for sample in samples.T])
+
+        return "pce success {}!".format(nsamp)
+
+@app.callback(
+    Output("example-output", "children"), [Input("table-button", "n_clicks")],Input('t1', 'data'),
+    Input('t1', 'columns'),Input('Ea-in', 'value'),Input('D0-in', 'value'),
+)
+def on_button_click(n,rows1,columns1,Ea,D0):
+    if n is None:
+        return "Not clicked."
+    else:
+        n_t_segs = 6
+        df1 = pd.DataFrame(rows1, columns=[c['name'] for c in columns1])
+        distance = (df1["distance"].tolist())
+        dr = (distance[2] - distance[1])/1e6
+        Lmax = max(distance)/1e6
+        dates = df1["date"].tolist()
+        tmax = max(dates)
+        tmin = min(dates)
+        dates= [dates]
+        errors =  [df1["error"].tolist()]
+        print(str(dates))
+        payload = {"function_to_run":"zonation","zon_n_t_segs": str(n_t_segs),
+            "Ea":str(Ea),"D0":str(D0),"U38Pb06":str(dates),"sigU38Pb06":str(errors),"Lmax":str(Lmax),"tmax":str(tmax),
+        "tmin":str(tmin),"dr":str(dr),"distance":str(distance)}
+
+        r = requests.post("http://0.0.0.0:8000/model", json=payload)
+        print(r.text)
+        return "200"
+
+@app.callback(
+    Output('t1_out', 'children'),
+    Input('t1', 'data'),
+    Input('t1', 'columns'))
+def display_output(rows, columns):
+    if rows is None:
+        raise PreventUpdate
+    df = pd.DataFrame(rows, columns=[c['name'] for c in columns])
+    result = json.dumps(df["date"].tolist())
+    return result
