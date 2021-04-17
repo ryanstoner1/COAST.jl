c1487a12817f345d678cbf47361a33e944e8b5cc
ryanstoner1 <rstoner@ucsb.edu>
2021-04-16 23:54:18 -0700

    Make module; parse boundaries and handle with JuMP

--- src/app.jl
+++ src/app.jl
@@ -1,5 +1,8 @@
-using JuMP
+
+
+module App
 using COAST
+using JuMP
 using JSON3
 using Genie
 using Ipopt
@@ -7,6 +10,7 @@ using Genie.Router
 using Genie.Requests
 using Genie.Renderer.Html
 
+export launchServer
 # string to say hello to users
 html_coast_introduction = """
     \n <p>COAST is a program for thermochronology sensitivity analysis and
@@ -28,6 +32,7 @@ function launchServer(progress_test,port)
    Genie.config.server_port = port
 
   route("/model", method = POST) do
+    print("running model POST setup \n")
     # extract payload from front-end
     payload = jsonpayload()
     #print(payload)
@@ -40,6 +45,7 @@ function launchServer(progress_test,port)
 
   # simple summary for website visits/GET requests
   route("/model", method = GET) do
+      print("test model get script \n")
       html_coast_introduction
   end
 
@@ -61,17 +67,18 @@ end
 # function to run in COAST
 function parse_and_run_payload!(queue,payload)
   id = payload["id"]
-  if id in queue["ids"]
-    return "Run already in progress!"
-  else
-    push!(queue["ids"],id)
-  end
+  # if id in queue["ids"]
+  #   return "Run already in progress!"
+  # else
+  #   push!(queue["ids"],id)
+  # end
   
   push!(queue["progress_percent"],0) 
   push!(queue["results"],Dict()) 
   function_to_COAST = payload["function_to_run"]
 
   if function_to_COAST == "zonation"
+    print("Running zonation script! \n")
     n_t = payload["zon_n_t_segs"]
     Ea = payload["Ea"]
     D0 = payload["D0"]
@@ -82,56 +89,50 @@ function parse_and_run_payload!(queue,payload)
     tmin = payload["tmin"]
     # 
     dr = payload["dr"]
-    distance = payload["distance"]
-    outstring = zonation_n_times_forward(distance,L,n_t,Ea,D0,U38_Pb06,sigU38_Pb06,dr,tmax,tmin)
+    distance = payload["distance"] # Vector
+    Tbound_top = payload["Tbound_top"]
+    Tbound_bot = payload["Tbound_bot"]
+    outstring = zonation_n_times_forward(distance,L,n_t,Ea,D0,U38_Pb06,sigU38_Pb06,dr,
+      tmax,tmin,Tbound_top,Tbound_bot)
     return outstring
   end
 
   return "Passing JSON payload to COAST successful!"
 end
 
-function zonation_n_times_forward(distance,L,n_t,Ea,D0,U38_Pb06,sigU38_Pb06,dr,tmax,tmin)
+function zonation_n_times_forward(distance,L,n_t,Ea,D0,U38_Pb06,sigU38_Pb06,dr,
+  tmax,tmin,Tbound_top,Tbound_bot)
   print("running_zonation_n_times_forward \n")
-  tmax = parse(Float64,tmax)
-  tmin = parse(Float64,tmin)
-  nt  = parse(Int64,n_t)
+  tmax = parse(Float64,tmax) # in yrs
+  tmin = parse(Float64,tmin) # in yrs
+  n_t  = parse(Int64,n_t)
   Ea = parse(Float64,Ea)
   D0 = parse(Float64,D0)
   dr = parse(Float64,dr)
   L = parse(Float64,L)
   distance = JSON3.read(distance, Vector{Float64})./1e6
-  U38_Pb06 = 1.0./JSON3.read(U38_Pb06, Vector{Vector{Float64}})
-  sigU38_Pb06 = 1.0./JSON3.read(sigU38_Pb06, Vector{Vector{Float64}})
-  U238 = 30.0
-  U235 = 0.0
-  Th232 = 0.0
-  nrad = round(Int,L/(dr))
-  # print(nrad,"\n")
-  # print(length(distance),"\n")
-  # print("$L \n")
-  # print("$U38_Pb06 \n")
-  t= collect(LinRange(tmax*COAST.sec_in_yrs,tmin*COAST.sec_in_yrs,nt))
-  nT = nt - 1
-  T= collect(LinRange(900.0,900.0,nT))
-  upper_bound_arr = (1200.0+273.0)*ones(nT)
-  lower_bound_arr = (600.0+273.0)*ones(nT)
+  sigU38_Pb06 = JSON3.read(sigU38_Pb06, Vector{Vector{Float64}})
+  Tbound_top = JSON3.read(Tbound_top, Vector{Float64})
+  Tbound_bot = JSON3.read(Tbound_bot, Vector{Float64})
+  U38_Pb06 = JSON3.read(U38_Pb06, Vector{Vector{Float64}})  
+  # rows are permutations of sample sets
+  n_constraints = length(U38_Pb06[1])
+
+  t = collect(LinRange(tmax*COAST.sec_in_yrs,tmin*COAST.sec_in_yrs,n_t))
+  upper_bound_arr = 420.0*ones(n_t)
+  upper_bound_arr[Tbound_top.>-1.0] = Tbound_top[Tbound_top.>-1.0]
+  lower_bound_arr = 273.0*ones(n_t)
+  lower_bound_arr[Tbound_bot.>-1.0] = Tbound_bot[Tbound_bot.>-1.0]
   model = Model(Ipopt.Optimizer)
-  JuMP.set_optimizer_attributes(model,"print_level"=>5,"tol"=>1e2,"linear_solver"=>"mumps","print_timing_statistics"=>"no")
-  (Tset,set_dev) = define_variables!(nT,1,model,T,upper_bound=upper_bound_arr,lower_bound=lower_bound_arr)
-  register(model,Symbol(zonation_diffusion),7+nT+nt,zonation_diffusion,autodiff=true)
-  register_objective_function!(nT,model)
+  JuMP.set_optimizer_attributes(model,"print_level"=>2,"tol"=>1e2,"linear_solver"=>"mumps","print_timing_statistics"=>"no")
+  register_objective_function!(n_t,model)
+  @NLparameter(model, pa[i=1:n_constraints] == U38_Pb06[1][i])
+  start_points = 0.5.*ones(Float64,n_t)
+  (T,set_dev) = define_variables!(n_t,n_constraints,model,start_points,upper_bound=upper_bound_arr,lower_bound=lower_bound_arr)
+  register_forward_model_zonation!(n_t,model)
+
   
-  nradplus = round(Int,75.0*1e-6/dr)
-  # U38_Pb06 = vcat(U38_Pb06[end].*ones(nradplus-nrad),U38_Pb06)
-  # print("$U38_Pb06 \n")
-  scaling = 1e7
-  # for jj = 1:nrad
-  #   constr1=@NLconstraint(model,[i = 1:1],(zonation_diffusion(Ea,D0,U238,0.0,0.0,L,nrad,t...,T...)-He_conc[i]*scaling)^2<=(0.01*He_conc[i]*scaling)^2)
-  # end
-    #set_objective_function!(model,Tset)
-  tT = vcat(t,T)
-  ratio = zonation_diffusion(Ea,D0,U238,U235,Th232,L,nrad,tT...)
-  return (n_t,Ea,ratio)
+  return lower_bound_arr
 end
 
 
@@ -143,12 +144,14 @@ function route_test_page_get_post!(progress_test,html_coast_introduction)
 
   # simple message to say hi
   route("/") do
+      print("Run get script COAST \n")
       html_coast_introduction
    end
 
    # use e.g. curl to test
    route("/", method = POST) do
      aa = jsonpayload()["name"]
+     print("testing progress script! \n")
      progress_test[1] += 1.0
      return Genie.Renderer.Json.json("Tested input: $(aa) ")
    end
@@ -161,7 +164,14 @@ function route_test_page_get_post!(progress_test,html_coast_introduction)
 
 end
 
+end # end module
 
-launchServer(progress_test,8000) # run if running locally
+# # run if running locally
 #launchServer(parse(Int, ARGS[1])) # run from dokku or heroku
 
+# if abspath(PROGRAM_FILE) == @__FILE__
+  
+#   print("running app!\n")
+#   progress_test = [0.0]
+#   App.launchServer(progress_test,8000)
+# end
