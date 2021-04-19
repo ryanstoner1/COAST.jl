f802537fa7badf67e23f8496aa0a765f0b62e3cc
ryanstoner1 <rstoner@ucsb.edu>
2021-04-19 00:35:50 -0700

    make server accessible dash

--- app.py
+++ app.py
@@ -3,3 +3,4 @@ import dash_bootstrap_components as dbc
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,title='COAST',suppress_callback_exceptions=True)
+server = app.server
