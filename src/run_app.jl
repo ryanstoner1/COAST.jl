7d820e90cc854840e1bd85e4dea2fcc29fd530b5
ryanstoner1 <rstoner@ucsb.edu>
2021-05-10 23:46:00 -0700

    Add setup to run locally

--- src/run_app.jl
+++ src/run_app.jl
@@ -5,5 +5,10 @@ include("COAST.jl")
 using .COAST
 
 progress_test = [0.0]
-COAST.App.launchServer(progress_test,parse(Int, ARGS[1]))
+try
+    COAST.App.launchServer(progress_test,parse(Int, ARGS[1]))
+catch e
+    print("Not on server . . . attempting to start locally \n")
+    COAST.App.launchServer(progress_test,8000)
+end
 
