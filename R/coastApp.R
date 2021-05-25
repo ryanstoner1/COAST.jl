132592091e711f3b2eee6a914eb2762013c8c568
ryanstoner1 <rstoner@ucsb.edu>
2021-05-24 23:06:01 -0700

    Allow forward model to call backend 2/2

--- R/coastApp.R
+++ R/coastApp.R
@@ -1,8 +1,8 @@
 # AUTO GENERATED FILE - DO NOT EDIT
 
-coastApp <- function(id=NULL, label=NULL, value=NULL) {
+coastApp <- function(id=NULL, D0L_a2=NULL, E_L=NULL, Etrap=NULL, L_dist=NULL, alpha=NULL, c0=NULL, c1=NULL, c2=NULL, c3=NULL, eta_q=NULL, label=NULL, omega=NULL, psi=NULL, rad=NULL, rmr0=NULL, th32=NULL, u38=NULL, value=NULL) {
     
-    props <- list(id=id, label=label, value=value)
+    props <- list(id=id, D0L_a2=D0L_a2, E_L=E_L, Etrap=Etrap, L_dist=L_dist, alpha=alpha, c0=c0, c1=c1, c2=c2, c3=c3, eta_q=eta_q, label=label, omega=omega, psi=psi, rad=rad, rmr0=rmr0, th32=th32, u38=u38, value=value)
     if (length(props) > 0) {
         props <- props[!vapply(props, is.null, logical(1))]
     }
@@ -10,7 +10,7 @@ coastApp <- function(id=NULL, label=NULL, value=NULL) {
         props = props,
         type = 'CoastApp',
         namespace = 'coast_app',
-        propNames = c('id', 'label', 'value'),
+        propNames = c('id', 'D0L_a2', 'E_L', 'Etrap', 'L_dist', 'alpha', 'c0', 'c1', 'c2', 'c3', 'eta_q', 'label', 'omega', 'psi', 'rad', 'rmr0', 'th32', 'u38', 'value'),
         package = 'coastApp'
         )
 
--- coast_app/CoastApp.py
+++ coast_app/CoastApp.py
@@ -16,18 +16,66 @@ Keyword arguments:
 - id (string; optional):
     The ID used to identify this component in Dash callbacks.
 
+- D0L_a2 (string; optional):
+    The value displayed in the input.
+
+- E_L (string; optional):
+    The value displayed in the input.
+
+- Etrap (string; optional):
+    The value displayed in the input.
+
+- L_dist (string; optional):
+    The value displayed in the input.
+
+- alpha (string; optional):
+    The value displayed in the input.
+
+- c0 (string; optional):
+    The value displayed in the input.
+
+- c1 (string; optional):
+    The value displayed in the input.
+
+- c2 (string; optional):
+    The value displayed in the input.
+
+- c3 (string; optional):
+    The value displayed in the input.
+
+- eta_q (string; optional):
+    The value displayed in the input.
+
 - label (string; required):
     A label that will be printed when this component is rendered.
+
+- omega (string; optional):
+    The value displayed in the input.
+
+- psi (string; optional):
+    The value displayed in the input.
+
+- rad (string; optional):
+    The value displayed in the input.
+
+- rmr0 (string; optional):
+    The value displayed in the input.
+
+- th32 (string; optional):
+    The value displayed in the input.
+
+- u38 (string; optional):
+    The value displayed in the input.
 
 - value (string; optional):
     The value displayed in the input."""
     @_explicitize_args
-    def __init__(self, id=Component.UNDEFINED, label=Component.REQUIRED, value=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'label', 'value']
+    def __init__(self, id=Component.UNDEFINED, label=Component.REQUIRED, value=Component.UNDEFINED, rad=Component.UNDEFINED, u38=Component.UNDEFINED, th32=Component.UNDEFINED, Etrap=Component.UNDEFINED, alpha=Component.UNDEFINED, c0=Component.UNDEFINED, c1=Component.UNDEFINED, c2=Component.UNDEFINED, c3=Component.UNDEFINED, rmr0=Component.UNDEFINED, eta_q=Component.UNDEFINED, L_dist=Component.UNDEFINED, psi=Component.UNDEFINED, omega=Component.UNDEFINED, E_L=Component.UNDEFINED, D0L_a2=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'D0L_a2', 'E_L', 'Etrap', 'L_dist', 'alpha', 'c0', 'c1', 'c2', 'c3', 'eta_q', 'label', 'omega', 'psi', 'rad', 'rmr0', 'th32', 'u38', 'value']
         self._type = 'CoastApp'
         self._namespace = 'coast_app'
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'label', 'value']
+        self.available_properties = ['id', 'D0L_a2', 'E_L', 'Etrap', 'L_dist', 'alpha', 'c0', 'c1', 'c2', 'c3', 'eta_q', 'label', 'omega', 'psi', 'rad', 'rmr0', 'th32', 'u38', 'value']
         self.available_wildcard_properties =            []
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
