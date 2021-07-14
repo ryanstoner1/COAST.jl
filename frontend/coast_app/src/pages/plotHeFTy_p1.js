48190ebe8ff45e125c074cb824159e21b1ce374c
ryanstoner1 <rstoner@ucsb.edu>
2021-07-14 13:22:37 -0700

    add file for HeFTy plotting

---
+++ frontend/coast_app/src/pages/plotHeFTy_p1.js
@@ -0,0 +1,56 @@
+
+const plotHeFTy = (maxTime, maxTemp, initP1) => {
+
+    const plotObj = {
+            tooltip: { 
+                animation:false,
+                enabled: false },
+            title: {
+                text: 'Temperature-Time Path',
+                align: 'center'
+            },
+            animation: false,
+            xAxis: {
+                title: { text: "time (Ma)" },
+                min: 0,
+                max: 1.2*maxTime,
+                reversed: true
+            },
+            yAxis: {
+                gridLineWidth: 0,
+                min: 0,
+                max: 1.2*maxTemp,
+                title: { text: "temperature (ºC)" },
+                reversed: true
+            },
+            series: initP1,
+            chart: {
+                plotBorderWidth: 2,
+                animation: false,
+            },
+            exporting: {
+                chartOptions: {
+                    plotOptions: {
+                        series: {
+                            dataLabels: {
+                                enabled: true
+                            }
+                        }
+                    }
+                }
+            },
+            plotOptions: {
+                series: {
+                    stickyTracking: false,
+                    allowPointSelect: false,
+                    marker: {
+                            enabled: false
+                    }
+                }
+            }
+        };
+        return plotObj
+};
+
+
+export default plotHeFTy
