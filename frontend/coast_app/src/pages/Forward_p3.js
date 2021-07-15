ryanstoner1/COAST.jl

commit a64c9e23bef4ba7d51d5dfee8393c2f3267c56b1

Handle context menu more robustly

frontend/coast_app/src/pages/Forward_p3.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react';
+import React, { useEffect, useState, useRef, useCallback} from 'react';
 import Highcharts from 'highcharts/highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import addExporting from "highcharts/modules/exporting";
@@ -7,6 +7,7 @@ import {ButtonGroup, ToggleButton, InputGroup, FormControl, Dropdown, DropdownBu
 import plotInit from './plotInit_p3.js'
 import plotInitXY from './plotInitXY_p3.js'
 import Menu from './Menu_p3.js'
+import {handleNonchartClick} from './p3/ContextMenu.js'
 import Diff from './diffusion_models_p3.js'
 import "./styles.css";
 addExporting(Highcharts);
@@ -17,31 +18,108 @@ require("highcharts/modules/draggable-points")(Highcharts);
 
 
 /**
- * ExampleComponent is an example component.
- * It takes a property, `label`, and
- * displays it.
- * It renders an input with the property `value`
- * which is editable by the user.
+ * Main app:
+ * used 
  */
 export default function CoastApp() {
     const [diffusionParams, setDiffusionParams] = useState({model: false});
     const [checkedList, setCheckedList] = useState([])
     const [maxChecked, setMaxChecked] = useState(false)
-    const [indPoint, setIndPoint] = useState(null);
     const [xPos, setXPos] = useState("0px");
     const [yPos, setYPos] = useState("0px");
     const [radioValue, setRadioValue] = useState('1');
 
-    const radios = [
-        { name: ' X-Y plot', value: '1' },
-        { name: ' Global sensitivity', value: '2' },
-    ];
     // only visible data
     const [xData,setXData] = useState([]);
     const [yData,setYData] = useState([]);
     const [menu, showMenu] = useState(false);
     const chartRef = useRef(null);   
     const [isChartXY, setIsChartXY] = useState(false); 
+
+    const radios = [
+        { name: ' X-Y plot', value: '1' },
+        { name: ' Global sensitivity', value: '2' },
+    ];
+    const [indPoint, setIndPoint] = useState(null);
+
+    const hideContextMenu = useCallback(() => {
+        showMenu(false);
+      }, [showMenu]);
+
+    const handleContextMenu = useCallback(
+            event => {
+            event.preventDefault();
+                setIndPoint(event.point.index);
+                setXPos(`${event.point.plotX}px`);
+                setYPos(`${event.point.plotY}px`);
+                showMenu(true);            
+            },
+            [showMenu, setXPos, setYPos, setIndPoint]
+        );
+
+    
+
+    const timeContextMenu = (e) => {
+        if (chartRef.current.chart.series[(2*indPoint+1)].visible===false) {
+            chartRef.current.chart.series[(2*indPoint+1)].show();
+            chartRef.current.chart.series[(2*indPoint+1)].options.dragDrop = {
+                draggableX: true, draggableY: false
+            };
+            
+            const xNew = [...chartRef.current.chart.series[(2*indPoint+1)].xData];
+            const yNew = [...chartRef.current.chart.series[(2*indPoint+1)].yData];
+            const xNewAdd = [{x: xNew[0], y: yNew[0]},{x: xNew[1], y: yNew[1]},{x: xNew[2], y: yNew[2]}];
+            
+
+            const maxCheckedCopy = maxChecked;
+
+            // errors if empty 
+            if (maxCheckedCopy=== true) {
+                setXData([...xData,{ind: indPoint, val: [...xNewAdd], check: false, disabled: true}]);
+            } else {
+                setXData([...xData,{ind: indPoint, val: [...xNewAdd], check: false, disabled: false}]);
+            }
+                    
+
+
+        } else {
+            chartRef.current.chart.series[(2*indPoint+1)].hide();
+            setXData(xData => xData.filter(value => value.ind!==(indPoint)));
+
+            chartRef.current.chart.series[(2*indPoint+1)].options.dragDrop = {
+                draggableX: false, draggableY: false
+            };
+        };
+    };
+
+
+    const temperatureContextMenu = (e) => {
+
+        if (chartRef.current.chart.series[(2*indPoint+2)].visible===false) {
+            chartRef.current.chart.series[(2*indPoint+2)].show();
+            chartRef.current.chart.series[(2*indPoint+2)].options.dragDrop = {
+                draggableX: false, draggableY: true
+            };
+            const xNew = [...chartRef.current.chart.series[(2*indPoint+2)].xData];
+            const yNew = [...chartRef.current.chart.series[(2*indPoint+2)].yData];
+            const yNewAdd = [{x: xNew[0], y: yNew[0]},{x: xNew[1], y: yNew[1]},{x: xNew[2], y: yNew[2]}];
+
+            const maxCheckedCopy = maxChecked;
+
+            // errors if empty 
+            if (maxCheckedCopy=== true) {
+                setYData([...yData,{ind: indPoint, val: [...yNewAdd], check: false, disabled: true}]);
+            } else {
+                setYData([...yData,{ind: indPoint, val: [...yNewAdd], check: false, disabled: false}]);
+            }
+        } else {
+            chartRef.current.chart.series[(2*indPoint+2)].hide();
+            chartRef.current.chart.series[(2*indPoint+2)].options.dragDrop = {
+                draggableX: false, draggableY: false
+            };
+            setYData(yData => yData.filter(value => value.ind!==(indPoint)));
+        };
+    };
 
     const initx1 = 20
     const inity1 = 30
@@ -261,23 +339,9 @@ export default function CoastApp() {
                          
         }
     };
-
-    const [options, setOptions] = useState(plotInit(initChartClick, initPointClick, pointDrag));
+    const options = plotInit(initChartClick, initPointClick, pointDrag);
+    //const [options, setOptions] = useState(plotInit(initChartClick, initPointClick, pointDrag));
     const optionsXY = plotInitXY(dataXY);
-    const hideContextMenu = useCallback(() => {
-        showMenu(false);
-      }, [showMenu]);
-
-    const handleContextMenu = useCallback(
-        event => {
-          event.preventDefault();
-            setIndPoint(event.point.index);
-            setXPos(`${event.point.plotX}px`);
-            setYPos(`${event.point.plotY}px`);
-            showMenu(true);            
-        },
-        [showMenu, setXPos, setYPos, setIndPoint]
-    );
     
     const handleXMin = (e,index,chartRef) => {        
         if (e.key === "Enter") {
@@ -369,74 +433,6 @@ export default function CoastApp() {
         window.removeEventListener('keydown', handleKeyDown);
     };
     }, [options.series]);
-    
-    const handleNonchartClick = (e) => {
-        if (!e.altKey) {
-            hideContextMenu();
-        }
-    };
-
-    const timeContextMenu = (e) => {
-        if (chartRef.current.chart.series[(2*indPoint+1)].visible===false) {
-            chartRef.current.chart.series[(2*indPoint+1)].show();
-            chartRef.current.chart.series[(2*indPoint+1)].options.dragDrop = {
-                draggableX: true, draggableY: false
-            };
-            
-            const xNew = [...chartRef.current.chart.series[(2*indPoint+1)].xData];
-            const yNew = [...chartRef.current.chart.series[(2*indPoint+1)].yData];
-            const xNewAdd = [{x: xNew[0], y: yNew[0]},{x: xNew[1], y: yNew[1]},{x: xNew[2], y: yNew[2]}];
-            
-
-            const maxCheckedCopy = maxChecked;
-
-            // errors if empty 
-            if (maxCheckedCopy=== true) {
-                setXData([...xData,{ind: indPoint, val: [...xNewAdd], check: false, disabled: true}]);
-            } else {
-                setXData([...xData,{ind: indPoint, val: [...xNewAdd], check: false, disabled: false}]);
-            }
-                    
-
-
-        } else {
-            chartRef.current.chart.series[(2*indPoint+1)].hide();
-            setXData(xData => xData.filter(value => value.ind!==(indPoint)));
-
-            chartRef.current.chart.series[(2*indPoint+1)].options.dragDrop = {
-                draggableX: false, draggableY: false
-            };
-        };
-    };
-
-
-    const temperatureContextMenu = (e) => {
-
-        if (chartRef.current.chart.series[(2*indPoint+2)].visible===false) {
-            chartRef.current.chart.series[(2*indPoint+2)].show();
-            chartRef.current.chart.series[(2*indPoint+2)].options.dragDrop = {
-                draggableX: false, draggableY: true
-            };
-            const xNew = [...chartRef.current.chart.series[(2*indPoint+2)].xData];
-            const yNew = [...chartRef.current.chart.series[(2*indPoint+2)].yData];
-            const yNewAdd = [{x: xNew[0], y: yNew[0]},{x: xNew[1], y: yNew[1]},{x: xNew[2], y: yNew[2]}];
-
-            const maxCheckedCopy = maxChecked;
- 
-            // errors if empty 
-            if (maxCheckedCopy=== true) {
-                setYData([...yData,{ind: indPoint, val: [...yNewAdd], check: false, disabled: true}]);
-            } else {
-                setYData([...yData,{ind: indPoint, val: [...yNewAdd], check: false, disabled: false}]);
-            }
-        } else {
-            chartRef.current.chart.series[(2*indPoint+2)].hide();
-            chartRef.current.chart.series[(2*indPoint+2)].options.dragDrop = {
-                draggableX: false, draggableY: false
-            };
-            setYData(yData => yData.filter(value => value.ind!==(indPoint)));
-        };
-    };
 
     const handleDiffModel = (e,diffModelType) => {
         setDiffusionParams({model: diffModelType})
@@ -552,9 +548,8 @@ export default function CoastApp() {
     };
 
 
-
         return (
-            <div id='testing2' onClick={handleNonchartClick}>
+            <div id='testing2' onClick={e => handleNonchartClick(e, hideContextMenu)}>
             <div>
             { menu ? <Menu xPos={xPos} yPos={yPos} timeContextMenu={timeContextMenu} temperatureContextMenu={temperatureContextMenu}/> : null }
             </div>
@@ -590,7 +585,7 @@ export default function CoastApp() {
                 <br></br>
                 { (diffusionParams.model==="flowers09") ?
                     <Diff chartRef={chartRef} xData={xData} yData={yData} checkedList={checkedList} maxChecked={maxChecked} onCheckChange={handleCheckFlowers09} 
-                        setIsChartXY={setIsChartXY} setDataXY={setDataXY}
+                        setIsChartXY={setIsChartXY} setDataXY={setDataXY} radioValue={radioValue}
                     />
                 : null}
                 { (diffusionParams.model==="cherniak00") ?

frontend/coast_app/src/pages/diffusion_models_p3.js
@@ -2,11 +2,12 @@ import {React, useState, useEffect} from 'react';
 import {InputGroup, FormControl, Button, Alert} from 'react-bootstrap';
 import axios from 'axios';
 
-const Diff = ({ chartRef, xData, yData, checkedList, maxChecked, onCheckChange, setIsChartXY, setDataXY} ) => {
+const Diff = ({ chartRef, xData, yData, checkedList, maxChecked, onCheckChange, setIsChartXY, setDataXY, radioValue} ) => {
     const [warnEmpty, setWarnEmpty] = useState(false)
     const [warnNonInt, setWarnNonInt] = useState(false)
     const [warnCheckedBad, setWarnCheckedBad] = useState(false)
-    const [numbertT, setNumbertT] = useState("")
+    const [numberX, setNumberX] = useState("")
+    const [numberZ, setNumberZ] = useState("")
     const [U238, setU238] = useState({main: '', min: '', max: ''})
     const [Th232, setTh232] = useState({main: '', min: '', max: ''})
     const [Ea, setEa] = useState({main: 122.3, min: '', max: ''})
@@ -25,10 +26,11 @@ const Diff = ({ chartRef, xData, yData, checkedList, maxChecked, onCheckChange, 
     const [etrap, setEtrap] = useState({main: 34, min: '', max: ''})
     // const [juliaResponse, setJuliaResponse] = useState({})
 
-    const handleProcess = (e) => {
+    const handleProcess = (e, radioVal) => {
         const formInit = {
             function_to_run: "store_params",
-            numbertT: numbertT,
+            numberX: numberX,
+            numberZ: numberZ,
             Letch: JSON.parse(JSON.stringify(Letch)),
             U238: U238,
             Th232: Th232,
@@ -47,6 +49,20 @@ const Diff = ({ chartRef, xData, yData, checkedList, maxChecked, onCheckChange, 
             etrap: etrap,
             userIP: undefined,
         };
+
+        const xDataCopy = [...xData];
+        const yDataCopy = [...yData];
+        xDataCopy.forEach((value,ind)=>{
+            // makes the processing easier on the python side
+            const newKeyString = "xData".concat(ind.toString())
+            formInit[newKeyString] = {min: value.val[0].x, main: value.val[1].x, max: value.val[2].x}
+        });
+        yDataCopy.forEach((value,ind)=>{
+            // makes the processing easier on the python side
+            const newKeyString = "yData".concat(ind.toString())
+            formInit[newKeyString] = {min: value.val[0].y, main: value.val[1].y, max: value.val[2].y}
+        });
+
         const formXYPlot = {
                 userIP: undefined,
                 xData: xData,
@@ -56,25 +72,45 @@ const Diff = ({ chartRef, xData, yData, checkedList, maxChecked, onCheckChange, 
                 checkedList: checkedList,
                 function_to_run: "run_xy_flowers09",
         }
-        console.log([...checkedList])
-        console.log(formInit.U238)
-        console.log(formInit["U238"].main)
-        // check inputs good before running request
-        if (numbertT==="") {
-            setWarnEmpty(true)
-        } else if (Number.isInteger(parseFloat(numbertT))===false) {
-            setWarnNonInt(true)
-        } else if (checkedList.length>0){
-            const checkedListCopy = [...checkedList];
-            checkedListCopy.forEach(value=>{
-                if ((formInit[value].min==="")| (formInit[value].max==="")) {
-                    setWarnCheckedBad(true) 
-                } else {
-                    setWarnCheckedBad(false) 
 
+        if (radioVal==="1") {
+            if (numberX==="") {
+                setWarnEmpty(true)
+            } else if (Number.isInteger(parseFloat(numberX))===false) {
+                setWarnNonInt(true)
+            } else if (checkedList.length>0){
+                const checkedListCopy = [...checkedList];
+                checkedListCopy.forEach(value=>{
+                    if ((formInit[value].min==="")| (formInit[value].max==="")) {
+                        setWarnCheckedBad(true) 
+                    } else {
+                        setWarnCheckedBad(false) 
+                    }
+                })
+                if (warnCheckedBad===false) {
+                    axios.get("http://127.0.0.1:5000/get_coast_ip")
+                    .then((res) => {
+                        formInit.userIP = res.data.ip;            
+                        return axios.post("http://0.0.0.0:8000/model", formInit)                       
+                    }).then(res=> {
+                        formXYPlot.userIP = formInit.userIP;
+                        return axios.post("http://0.0.0.0:8000/model", formXYPlot)           
+                    }).then(res=>{
+                        setIsChartXY(true)
+                        const dataout = res.data[0]
+                        console.log(dataout)
+                        const xout = res.data[1]
+                        console.log(xout)
+                        const dataHighchartsXY = dataout.map(value=>{
+                            return value.map((value_inner,ind)=>{
+                                const returnval = {x:xout[ind], y:value_inner }
+                                return returnval
+                            })
+                        })
+                        setDataXY(dataHighchartsXY)
+                    }).catch(err => console.warn(err));                
                 }
-            })
-            if (warnCheckedBad===false) {
+            } else {
                 axios.get("http://127.0.0.1:5000/get_coast_ip")
                 .then((res) => {
                     formInit.userIP = res.data.ip;            
@@ -85,34 +121,40 @@ const Diff = ({ chartRef, xData, yData, checkedList, maxChecked, onCheckChange, 
                 }).then(res=>{
                     setIsChartXY(true)
                     console.log(res.data)
-                    setDataXY(res.data)
-                }).catch(err => console.warn(err));                
-            }
+                    
+                    const dataHighchartsXY = res.data.map(value=>{
+                        return value.map((value_inner,ind)=>{
+                            const returnval = {x:chartRef.current.chart.series[0].xData[ind], y:value_inner }
+                            console.log(returnval)
+                            return returnval
+                        })
+                    })
+                    setDataXY(dataHighchartsXY)
+                }).catch(err => console.warn(err));
+            }            
         } else {
-            axios.get("http://127.0.0.1:5000/get_coast_ip")
-            .then((res) => {
-                formInit.userIP = res.data.ip;            
-                return axios.post("http://0.0.0.0:8000/model", formInit)                       
-            }).then(res=> {
-                formXYPlot.userIP = formInit.userIP;
-                return axios.post("http://0.0.0.0:8000/model", formXYPlot)           
-            }).then(res=>{
-                setIsChartXY(true)
-                setDataXY(res.data)
-                console.log(res.data)
-            }).catch(err => console.warn(err));
-        } 
+            const formData = new FormData();
+            formData.append("param1",JSON.stringify(formInit));   
+            const config = {     
+                headers: { 'content-type': 'application/json' }
+            }
+            axios
+            .post("http://127.0.0.1:5000/getPCE", formData, config)
+            .then(res => console.log(res))
+            .catch(err => console.warn(err))
+        }
         };
 
         // get rid of warnings when user modifies previously incorrect entry
         useEffect(value=>{
             setWarnEmpty(false)
             setWarnNonInt(false)
-        },[numbertT])
+        },[numberX,numberZ])
+
 
     return(
     <div>
-    <Button variant="outline-primary" onClick={handleProcess}>Start sensitivity analysis</Button>
+    <Button variant="outline-primary" onClick={e => handleProcess(e,radioValue)}>Start sensitivity analysis</Button>
     <br></br>
     <br></br>
     { warnEmpty ? <Alert variant={"warning"}>COAST  was unable to process data! Set the number of time-temperature (t-T) paths to solve for before processing.</Alert> : null}
@@ -125,7 +167,8 @@ const Diff = ({ chartRef, xData, yData, checkedList, maxChecked, onCheckChange, 
         <InputGroup.Prepend>
         <InputGroup.Text id="inputGroup-npaths">number tT<sub>paths </sub><div>&nbsp;</div><input type="checkbox"   value=" Bike"/></InputGroup.Text>
         </InputGroup.Prepend>
-        <FormControl aria-label="Text input with checkbox" placeholder="total counts (<200,000 single grain, <20,000 profile)" type="number" value={numbertT} onInput={e => setNumbertT(e.target.value)}/>
+        <FormControl aria-label="Text input with checkbox" placeholder="n runs x axis (<100,000 single grain, <15,000 profile)" type="number" value={numberX} onInput={e => setNumberX(e.target.value)}/>
+        <FormControl aria-label="Text input with checkbox" placeholder="n runs countour (<100,000 single grain, <15,000 profile)" type="number" value={numberZ} onInput={e => setNumberZ(e.target.value)}/>
     </InputGroup>                    
     <h5>Diffusion parameters</h5>
     <InputGroup className="mb-3">

frontend/coast_app/src/pages/p3/ContextMenu.js
@@ -1,0 +1,10 @@
+
+import {useCallback, useState} from 'react';
+
+export const handleNonchartClick = (e, hideContextMenu) => {
+    if (!e.altKey) {
+        hideContextMenu();
+    }
+};
+
+// export default ContextMenu

commit 6aaf9333afe86506848f30cf396a0e4cd767cc89

Fix contextmenu align bug caused by a64c9e23bef4ba

frontend/coast_app/src/pages/Forward_p3.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react';
+import React, { useEffect, useState, useRef, useCallback} from 'react';
 import Highcharts from 'highcharts/highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import addExporting from "highcharts/modules/exporting";
@@ -17,11 +17,8 @@ require("highcharts/modules/draggable-points")(Highcharts);
 
 
 /**
- * ExampleComponent is an example component.
- * It takes a property, `label`, and
- * displays it.
- * It renders an input with the property `value`
- * which is editable by the user.
+ * Main app:
+ * used 
  */
 export default function CoastApp() {
     const [diffusionParams, setDiffusionParams] = useState({model: false});
@@ -32,10 +29,6 @@ export default function CoastApp() {
     const [yPos, setYPos] = useState("0px");
     const [radioValue, setRadioValue] = useState('1');
 
-    const radios = [
-        { name: ' X-Y plot', value: '1' },
-        { name: ' Global sensitivity', value: '2' },
-    ];
     // only visible data
     const [xData,setXData] = useState([]);
     const [yData,setYData] = useState([]);
@@ -43,6 +36,86 @@ export default function CoastApp() {
     const chartRef = useRef(null);   
     const [isChartXY, setIsChartXY] = useState(false); 
 
+    const radios = [
+        { name: ' X-Y plot', value: '1' },
+        { name: ' Global sensitivity', value: '2' },
+    ];
+    const hideContextMenu = useCallback(() => {
+        showMenu(false);
+    }, [showMenu]);
+
+    const handleContextMenu = useCallback(
+        event => {
+          event.preventDefault();
+            setIndPoint(event.point.index);
+            setXPos(`${event.point.plotX}px`);
+            setYPos(`${event.point.plotY}px`);
+            showMenu(true);            
+        },
+        [showMenu, setXPos, setYPos, setIndPoint]
+    );
+
+    const timeContextMenu = (e) => {
+        if (chartRef.current.chart.series[(2*indPoint+1)].visible===false) {
+            chartRef.current.chart.series[(2*indPoint+1)].show();
+            chartRef.current.chart.series[(2*indPoint+1)].options.dragDrop = {
+                draggableX: true, draggableY: false
+            };
+            
+            const xNew = [...chartRef.current.chart.series[(2*indPoint+1)].xData];
+            const yNew = [...chartRef.current.chart.series[(2*indPoint+1)].yData];
+            const xNewAdd = [{x: xNew[0], y: yNew[0]},{x: xNew[1], y: yNew[1]},{x: xNew[2], y: yNew[2]}];
+            
+
+            const maxCheckedCopy = maxChecked;
+
+            // errors if empty 
+            if (maxCheckedCopy=== true) {
+                setXData([...xData,{ind: indPoint, val: [...xNewAdd], check: false, disabled: true}]);
+            } else {
+                setXData([...xData,{ind: indPoint, val: [...xNewAdd], check: false, disabled: false}]);
+            }
+                    
+
+
+        } else {
+            chartRef.current.chart.series[(2*indPoint+1)].hide();
+            setXData(xData => xData.filter(value => value.ind!==(indPoint)));
+
+            chartRef.current.chart.series[(2*indPoint+1)].options.dragDrop = {
+                draggableX: false, draggableY: false
+            };
+        };
+    };
+
+
+    const temperatureContextMenu = (e) => {
+
+        if (chartRef.current.chart.series[(2*indPoint+2)].visible===false) {
+            chartRef.current.chart.series[(2*indPoint+2)].show();
+            chartRef.current.chart.series[(2*indPoint+2)].options.dragDrop = {
+                draggableX: false, draggableY: true
+            };
+            const xNew = [...chartRef.current.chart.series[(2*indPoint+2)].xData];
+            const yNew = [...chartRef.current.chart.series[(2*indPoint+2)].yData];
+            const yNewAdd = [{x: xNew[0], y: yNew[0]},{x: xNew[1], y: yNew[1]},{x: xNew[2], y: yNew[2]}];
+
+            const maxCheckedCopy = maxChecked;
+ 
+            // errors if empty 
+            if (maxCheckedCopy=== true) {
+                setYData([...yData,{ind: indPoint, val: [...yNewAdd], check: false, disabled: true}]);
+            } else {
+                setYData([...yData,{ind: indPoint, val: [...yNewAdd], check: false, disabled: false}]);
+            }
+        } else {
+            chartRef.current.chart.series[(2*indPoint+2)].hide();
+            chartRef.current.chart.series[(2*indPoint+2)].options.dragDrop = {
+                draggableX: false, draggableY: false
+            };
+            setYData(yData => yData.filter(value => value.ind!==(indPoint)));
+        };
+    };
     const initx1 = 20
     const inity1 = 30
     const dataXY1 = [{x:initx1, y:inity1+15}, {x:initx1+50, y:inity1+105}, {x:initx1+105, y:inity1+205}];
@@ -264,20 +337,6 @@ export default function CoastApp() {
 
     const [options, setOptions] = useState(plotInit(initChartClick, initPointClick, pointDrag));
     const optionsXY = plotInitXY(dataXY);
-    const hideContextMenu = useCallback(() => {
-        showMenu(false);
-      }, [showMenu]);
-
-    const handleContextMenu = useCallback(
-        event => {
-          event.preventDefault();
-            setIndPoint(event.point.index);
-            setXPos(`${event.point.plotX}px`);
-            setYPos(`${event.point.plotY}px`);
-            showMenu(true);            
-        },
-        [showMenu, setXPos, setYPos, setIndPoint]
-    );
     
     const handleXMin = (e,index,chartRef) => {        
         if (e.key === "Enter") {
@@ -374,68 +433,6 @@ export default function CoastApp() {
         if (!e.altKey) {
             hideContextMenu();
         }
-    };
-
-    const timeContextMenu = (e) => {
-        if (chartRef.current.chart.series[(2*indPoint+1)].visible===false) {
-            chartRef.current.chart.series[(2*indPoint+1)].show();
-            chartRef.current.chart.series[(2*indPoint+1)].options.dragDrop = {
-                draggableX: true, draggableY: false
-            };
-            
-            const xNew = [...chartRef.current.chart.series[(2*indPoint+1)].xData];
-            const yNew = [...chartRef.current.chart.series[(2*indPoint+1)].yData];
-            const xNewAdd = [{x: xNew[0], y: yNew[0]},{x: xNew[1], y: yNew[1]},{x: xNew[2], y: yNew[2]}];
-            
-
-            const maxCheckedCopy = maxChecked;
-
-            // errors if empty 
-            if (maxCheckedCopy=== true) {
-                setXData([...xData,{ind: indPoint, val: [...xNewAdd], check: false, disabled: true}]);
-            } else {
-                setXData([...xData,{ind: indPoint, val: [...xNewAdd], check: false, disabled: false}]);
-            }
-                    
-
-
-        } else {
-            chartRef.current.chart.series[(2*indPoint+1)].hide();
-            setXData(xData => xData.filter(value => value.ind!==(indPoint)));
-
-            chartRef.current.chart.series[(2*indPoint+1)].options.dragDrop = {
-                draggableX: false, draggableY: false
-            };
-        };
-    };
-
-
-    const temperatureContextMenu = (e) => {
-
-        if (chartRef.current.chart.series[(2*indPoint+2)].visible===false) {
-            chartRef.current.chart.series[(2*indPoint+2)].show();
-            chartRef.current.chart.series[(2*indPoint+2)].options.dragDrop = {
-                draggableX: false, draggableY: true
-            };
-            const xNew = [...chartRef.current.chart.series[(2*indPoint+2)].xData];
-            const yNew = [...chartRef.current.chart.series[(2*indPoint+2)].yData];
-            const yNewAdd = [{x: xNew[0], y: yNew[0]},{x: xNew[1], y: yNew[1]},{x: xNew[2], y: yNew[2]}];
-
-            const maxCheckedCopy = maxChecked;
- 
-            // errors if empty 
-            if (maxCheckedCopy=== true) {
-                setYData([...yData,{ind: indPoint, val: [...yNewAdd], check: false, disabled: true}]);
-            } else {
-                setYData([...yData,{ind: indPoint, val: [...yNewAdd], check: false, disabled: false}]);
-            }
-        } else {
-            chartRef.current.chart.series[(2*indPoint+2)].hide();
-            chartRef.current.chart.series[(2*indPoint+2)].options.dragDrop = {
-                draggableX: false, draggableY: false
-            };
-            setYData(yData => yData.filter(value => value.ind!==(indPoint)));
-        };
     };
 
     const handleDiffModel = (e,diffModelType) => {