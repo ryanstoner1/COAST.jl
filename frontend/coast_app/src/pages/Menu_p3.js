eb0bfde197e8be6e78283682554df9061dc63e9a
ryanstoner1 <rstoner@ucsb.edu>
2021-09-04 00:41:49 -0700

    improve context menu func names 2/2

--- frontend/coast_app/src/pages/Menu_p3.js
+++ frontend/coast_app/src/pages/Menu_p3.js
@@ -5,14 +5,14 @@ import horiz from './horiz.svg'
 const Menu = ({xPos,yPos,
     chartRef,indPoint,maxChecked,
     xData,yData,setXData,setYData,
-    timeContextMenu,temperatureContextMenu}) => {
+    handleTimeSelectMenu, handleTemperatureSelectMenu}) => {
     const xPosRaw = xPos.slice(0,xPos.length-2);
     const xPosNew = parseInt(xPosRaw) + 10;
     const yPosRaw = yPos.slice(0,yPos.length-2);
     const yPosNew = parseInt(yPosRaw) - 3;
     return (
       <ul className="menu" style={{ top: yPosNew, left: xPosNew }}>
-        <li onClick={(e)=>timeContextMenu(e, chartRef, indPoint, maxChecked, xData, setXData)}>
+        <li onClick={()=>handleTimeSelectMenu(chartRef, indPoint, maxChecked, xData, setXData)}>
             <div className='container'>
                 <span id="a">
                     toggle time bounds (Ma) 
@@ -20,7 +20,7 @@ const Menu = ({xPos,yPos,
                 <img src={horiz} alt='' width='30px'  id="b"></img>
             </div>
         </li>
-        <li onClick={(e)=>temperatureContextMenu(e, chartRef, indPoint, maxChecked, yData, setYData)}>
+        <li onClick={()=>handleTemperatureSelectMenu(chartRef, indPoint, maxChecked, yData, setYData)}>
             <div className="container">
                 <span id="c">toggle temperature bounds (ºC) </span>
                 <img src={vert} alt='' width='30px' id="d"></img>
