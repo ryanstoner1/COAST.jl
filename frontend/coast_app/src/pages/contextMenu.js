3907da71f5b902ad9bc08bdbd738ecb48be0d152
ryanstoner1 <rstoner@ucsb.edu>
2021-09-13 15:24:12 -0700

    Make more intuitive

--- frontend/coast_app/src/pages/contextMenu.js
+++ frontend/coast_app/src/pages/contextMenu.js
@@ -1,27 +1,27 @@
 // dropdown functionality when alt-clicking point on chart
 // either adds or gets rid of errorbars 
-const handleTimeSelectMenu = (chartRef, indPoint, maxChecked, xData, setXData) => {
+const handleTimeSelectMenu = (chartRef, indPoint, maxChecked, tData, settData) => {
     if (chartRef.current.chart.series[(2*indPoint+1)].visible===false) {
         chartRef.current.chart.series[(2*indPoint+1)].show();
         chartRef.current.chart.series[(2*indPoint+1)].options.dragDrop = {
             draggableX: true, draggableY: false
         };
         
-        const xNew = [...chartRef.current.chart.series[(2*indPoint+1)].xData];
-        const yNew = [...chartRef.current.chart.series[(2*indPoint+1)].yData];
-        const xNewAdd = [{x: xNew[0], y: yNew[0]},{x: xNew[1], y: yNew[1]},{x: xNew[2], y: yNew[2]}];
+        const tNew = [...chartRef.current.chart.series[(2*indPoint+1)].xData];
+        const TNew = [...chartRef.current.chart.series[(2*indPoint+1)].yData];
+        const tNewAdd = [{x: tNew[0], y: TNew[0]},{x: tNew[1], y: TNew[1]},{x: tNew[2], y: TNew[2]}];
         const maxCheckedCopy = maxChecked;
 
         // errors if empty 
         if (maxCheckedCopy=== true) {
-            setXData([...xData,{ind: indPoint, val: [...xNewAdd], check: false, disabled: true}]);
+            settData([...tData,{ind: indPoint, val: [...tNewAdd], check: false, disabled: true}]);
         } else {
-            setXData([...xData,{ind: indPoint, val: [...xNewAdd], check: false, disabled: false}]);
+            settData([...tData,{ind: indPoint, val: [...tNewAdd], check: false, disabled: false}]);
         }
                 
     } else {
         chartRef.current.chart.series[(2*indPoint+1)].hide();
-        setXData(xData => xData.filter(value => value.ind!==(indPoint)));
+        settData(tData => tData.filter(value => value.ind!==(indPoint)));
 
         chartRef.current.chart.series[(2*indPoint+1)].options.dragDrop = {
             draggableX: false, draggableY: false
@@ -30,7 +30,7 @@ const handleTimeSelectMenu = (chartRef, indPoint, maxChecked, xData, setXData) =
 };
 
 
-const handleTemperatureSelectMenu = (chartRef, indPoint, maxChecked, yData, setYData) => {
+const handleTemperatureSelectMenu = (chartRef, indPoint, maxChecked, TData, setTData) => {
 
     if (chartRef.current.chart.series[(2*indPoint+2)].visible===false) {
         chartRef.current.chart.series[(2*indPoint+2)].show();
@@ -45,16 +45,16 @@ const handleTemperatureSelectMenu = (chartRef, indPoint, maxChecked, yData, setY
 
         // errors if empty 
         if (maxCheckedCopy=== true) {
-            setYData([...yData,{ind: indPoint, val: [...yNewAdd], check: false, disabled: true}]);
+            setTData([...TData,{ind: indPoint, val: [...yNewAdd], check: false, disabled: true}]);
         } else {
-            setYData([...yData,{ind: indPoint, val: [...yNewAdd], check: false, disabled: false}]);
+            setTData([...TData,{ind: indPoint, val: [...yNewAdd], check: false, disabled: false}]);
         }
     } else {
         chartRef.current.chart.series[(2*indPoint+2)].hide();
         chartRef.current.chart.series[(2*indPoint+2)].options.dragDrop = {
             draggableX: false, draggableY: false
         };
-        setYData(yData => yData.filter(value => value.ind!==(indPoint)));
+        setTData(TData => TData.filter(value => value.ind!==(indPoint)));
     };
 };
 
